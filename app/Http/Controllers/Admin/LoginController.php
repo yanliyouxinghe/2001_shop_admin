991122da1e6a13b1cd21f01df70d1473661e1203
dong <1123370957@qq.com>
2020-11-13 20:56:29 +0800

    解决的冲突

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -23,9 +23,12 @@ class LoginController extends Controller
         if(!$admin){
             return redirect('/login')->with('msg','用户名或密码错误');
         }
-        if(decrypt($admin->admin_pwd)!=$post['admin_pwd']){
-            return redirect('/login')->with('msg','用户名或密码错误');
-        }
+        // if(decrypt($admin->admin_pwd)!=$post['admin_pwd']){
+        //     return redirect('/login')->with('msg','用户名或密码错误');
+        // }
+        if (!Hash::check($post['admin_pwd'],$admin['admin_pwd'])) {
+            return redirect('/login')->with('msg',"用户名或密码错误");
+         }
         session(['admin_name'=>$admin->admin_name]);
         return redirect('/');
     }
--- app/Model/AdminModel.php
+++ app/Model/AdminModel.php
@@ -10,36 +10,8 @@ class AdminModel extends Model
      protected $table = 'sh_admin';
      //指定主键
      protected $primaryKey = 'admin_id';
-<<<<<<< HEAD
      //不自动添加时间 
      public $timestamps = false;
      //黑名单
      protected $guarded=[];
 }
-=======
-     //不自动添加时间 create_at update_at
-     public $timestamps = false;
-     //黑名单
-     protected $guarded=[];
-
-
-     public function create_data($data){
-        $data['admin_pwd']=password_hash($data['admin_pwd'],PASSWORD_DEFAULT);
-         $data=self::create($data);
-         if($data){
-            return redirect('admin.list');
-         }
-         
-         
-     }
-     public function list_data(){
-         $data=self::orderBy('admin_id','desc')->paginate(3);;
-         return $data;
-     }
-     public function destroy_date($admin_id){
-            $res=self::destroy($admin_id);
-            return $res;
-     }
-}
-
->>>>>>> wang
