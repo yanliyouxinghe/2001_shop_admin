5effb3282dc69b120e2427680f185405ea2f6edd
jiayonglong <2451156409@qq.com>
2020-12-08 21:23:41 +0800

    js验证

--- routes/web.php
+++ routes/web.php
@@ -105,7 +105,7 @@ Route::prefix('/notice')->group(function(){
   Route::get('/list','Admin\NoticeController@index')->name('notice.list');//公告展示
   Route::post('/store','Admin\NoticeController@store')->name('notice.store');
   Route::any('/uploads','Admin\NoticeController@uploads')->name('notice.uploads');
-  Route::get('/destroy/{id}','Admin\NoticeController@destroy')->name('notice.destroy');//公告删除
+  Route::get('/destroy','Admin\NoticeController@destroy')->name('notice.destroy');//公告删除
   // Route::get('/show/{id}','Admin\NoticeController@show')->name('notice.show');
   Route::get('/edit/{id}','Admin\NoticeController@edit')->name('notice.edit');
  Route::post('/update/{id}','Admin\NoticeController@update')->name('notice.update');//公告执行修改
